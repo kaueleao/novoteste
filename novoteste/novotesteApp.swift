41653cb26bf9b218d8b6c237bfccbb0ffab5be75
kaueleao <kaue44@yahoo.com.br>
2023-01-06 15:10:46 -0300

    Initial Commit

---
+++ novoteste/novotesteApp.swift
@@ -0,0 +1,17 @@
+//
+//  novotesteApp.swift
+//  novoteste
+//
+//  Created by Kauê Leão on 06/01/23.
+//
+
+import SwiftUI
+
+@main
+struct novotesteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
